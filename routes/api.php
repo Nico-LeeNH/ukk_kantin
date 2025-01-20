097e413efa0cace353ba4ec0b1b4d3e2c6f94172
Nico Lee <new.nikolee2007@gmail.com>
2025-01-21 06:46:34 +0700

    feat: add siswa, menu, menu diskon, transaksi and detail transaksi

--- routes/api.php
+++ routes/api.php
@@ -29,16 +29,18 @@ Route::post('/add', [SiswaController::class, 'create']);
 Route::put('/update/{id}', [SiswaController::class, 'update']);
 Route::delete('/delete/{id}', [SiswaController::class, 'delete']);
 Route::get('/getmenu', [SiswaController::class, 'getMenu']);
+Route::get('/getstatuspesan/{id}', [SiswaController::class, 'getstatuspesan']);
+Route::get('/gethistorytransaksi/{id_siswa}/{month}', [SiswaController::class, 'getTransaksiByMonth']);
 
 Route::get('/getadmin', [AdminSController::class, 'getadmin']);
 Route::post('/addadmin', [AdminSController::class, 'createadmin']);
 Route::put('/updateadmin/{id}', [AdminSController::class, 'updateadmin']);
 Route::delete('/deleteadmin/{id}', [AdminSController::class, 'deleteadmin']);
 
-Route::get('/get', [MenuController::class, 'getmenu']);
-Route::post('/add', [MenuController::class, 'createmenu']);
-Route::put('/update/{id}', [MenuController::class, 'updatemenu']);
-Route::delete('/delete/{id}', [MenuController::class, 'deletemenu']);
+Route::get('/getmenu', [MenuController::class, 'getmenu']);
+Route::post('/addmenu', [MenuController::class, 'createmenu']);
+Route::put('/updatemenu/{id}', [MenuController::class, 'updatemenu']);
+Route::delete('/deletemenu/{id}', [MenuController::class, 'deletemenu']);
 
 Route::get('/diskon', [DiskonController::class, 'get']);
 Route::post('/diskon', [DiskonController::class, 'creatediskon']);
@@ -46,9 +48,14 @@ Route::put('/diskon/{id}', [DiskonController::class, 'updatediskon']);
 Route::delete('/diskon/{id}', [DiskonController::class, 'deletediskon']);
 
 Route::post('/diskonmenu', [MenuDiskonController::class, 'creatediskonmenu']);
+Route::get('/getmenudiskon', [MenuDiskonController::class, 'getmenudiskon']);
 
 Route::get('/gettransaksi', [TransaksiController::class, 'gettransaksi']);
+Route::get('/transaksi/{month}/{year}', [TransaksiController::class, 'getTransaksiByMonth']);
 Route::post('/addtransaksi', [TransaksiController::class, 'transaksi']);
+Route::put('/updatetransaksi/{id}', [TransaksiController::class, 'updatetransaksi']);
+Route::put('/updatestatus/{id}', [TransaksiController::class, 'updateStatus']);
 
 Route::get('/getdetailtransaksi', [DetailTransaksiController::class, 'getdetailtransaksi']);
+Route::get('/rekappemasukan/{month}/{year}', [DetailTransaksiController::class, 'getRekapPemasukanByMonth']);
 Route::post('/detailtransaksi', [DetailTransaksiController::class, 'detailtransaksi']);
