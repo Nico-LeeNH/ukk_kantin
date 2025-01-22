40b65682cfd8742c99f57296bd0f52a81e3babd8
Nico Lee <new.nikolee2007@gmail.com>
2025-01-22 13:48:39 +0700

    commit

--- routes/api.php
+++ routes/api.php
@@ -10,6 +10,7 @@ use App\Http\Controllers\MenuDiskonController;
 use App\Http\Controllers\SiswaController;
 use App\Http\Controllers\TransaksiController;
 use App\Http\Middleware\JwtMiddleware;
+use App\Models\DetailTransaksi;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -23,18 +24,23 @@ Route::post('/login', [AuthController::class, 'login']);
 Route::middleware(['auth:api'])->group(function () {
     Route::post('/logout', [AuthController::class, 'logout']);
 });
-
+Route::middleware(['auth:api', \App\Http\Middleware\AddUserIdFromRole::class])->group(function () {
+    Route::post('/siswa', [SiswaController::class, 'create']);
+    Route::put('/siswa/{id}', [SiswaController::class, 'update']);
+    Route::post('/addadmin', [AdminSController::class, 'createadmin']);
+    Route::put('/updateadmin/{id}', [AdminSController::class, 'updateadmin']);
+});
 Route::get('/get', [SiswaController::class, 'get']);
-Route::post('/add', [SiswaController::class, 'create']);
-Route::put('/update/{id}', [SiswaController::class, 'update']);
 Route::delete('/delete/{id}', [SiswaController::class, 'delete']);
 Route::get('/getmenu', [SiswaController::class, 'getMenu']);
 Route::get('/getstatuspesan/{id}', [SiswaController::class, 'getstatuspesan']);
 Route::get('/gethistorytransaksi/{id_siswa}/{month}', [SiswaController::class, 'getTransaksiByMonth']);
+Route::get('/cetak-nota/{id_transaksi}', [DetailTransaksiController::class, 'cetakNota']);
+Route::middleware(['auth:api', \App\Http\Middleware\CheckRoleSiswa::class,])->group(function () {
+    Route::get('/cetak-notas/{id_transaksi}', [SiswaController::class, 'cetakNotas']);
+});
 
 Route::get('/getadmin', [AdminSController::class, 'getadmin']);
-Route::post('/addadmin', [AdminSController::class, 'createadmin']);
-Route::put('/updateadmin/{id}', [AdminSController::class, 'updateadmin']);
 Route::delete('/deleteadmin/{id}', [AdminSController::class, 'deleteadmin']);
 
 Route::get('/getmenu', [MenuController::class, 'getmenu']);
@@ -50,9 +56,11 @@ Route::delete('/diskon/{id}', [DiskonController::class, 'deletediskon']);
 Route::post('/diskonmenu', [MenuDiskonController::class, 'creatediskonmenu']);
 Route::get('/getmenudiskon', [MenuDiskonController::class, 'getmenudiskon']);
 
+Route::middleware(['auth:api', \App\Http\Middleware\AddSiswaId::class,])->group(function () {
+    Route::post('/transaksi', [TransaksiController::class, 'transaksi']);
+});
 Route::get('/gettransaksi', [TransaksiController::class, 'gettransaksi']);
 Route::get('/transaksi/{month}/{year}', [TransaksiController::class, 'getTransaksiByMonth']);
-Route::post('/addtransaksi', [TransaksiController::class, 'transaksi']);
 Route::put('/updatetransaksi/{id}', [TransaksiController::class, 'updatetransaksi']);
 Route::put('/updatestatus/{id}', [TransaksiController::class, 'updateStatus']);
 
