ccc9b2e9bda7048d58180a4299df19edbe404416
Nico Lee <new.nikolee2007@gmail.com>
2025-01-30 14:52:34 +0700

    feat: cek role admin

---
+++ app/Http/Middleware/CheckRoleAdmin.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Symfony\Component\HttpFoundation\Response;
+
+class CheckRoleAdmin
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        $user = Auth::user();
+
+        if ($user && $user->role === 'admin_stan') { // Pastikan pengguna memiliki peran siswa
+            return $next($request);
+        }
+
+        return response()->json([
+            'status' => false,
+            'message' => 'Unauthorized'
+        ], 403);
+        // return $next($request);
+    }
+}
